7f8c9c2f52920e2f191c0e9b25ef2bc8a3bd9c3f
Rayya Mahira <rayraeilish@gmail.com>
2024-09-10 14:45:18 +0700

    connection setup

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+// target done = akhir bulan oct
+
+$conn = mysqli_connect("localhost","root","","sch_kasir_seveneleven");
+// if (mysqli_connect_errno()) {
+//     printf("error", mysqli_connect_error());
+// }
+?>
